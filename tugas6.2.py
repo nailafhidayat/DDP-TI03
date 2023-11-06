fa0f33d73238375c86a687ebe8d8fec711a51298
nailafhidayat <146088274+nailafhidayat@users.noreply.github.com>
2023-11-06 05:14:14 -0800

    Add files via upload

---
+++ tugas6.2.py
@@ -0,0 +1,14 @@
+a = 1
+while a <= 19:
+        print(a)
+        a += 2
+
+#variabel untuk menyimpan hasil penjumlahan
+total = 0
+
+#loop melalui bilangan ganjil dari 1 hingga 19
+for bilangan in range(1,20,2):
+    total += bilangan
+
+#menampilkan hasil penjumlahan
+print("Hasil penjumlahan: ", total)
