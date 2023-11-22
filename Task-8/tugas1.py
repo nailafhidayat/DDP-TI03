78d28e1c634ea4307a1afbd0e3480abaf884e41e
nailafhidayat <146088274+nailafhidayat@users.noreply.github.com>
2023-11-22 10:10:19 +0700

    Create tugas1.py

---
+++ Task-8/tugas1.py
@@ -0,0 +1,10 @@
+# deklarasi fungsi
+def profil(nama,alamat,gender,umur,hoby):
+    print("Hallo nama saya",nama)
+    print("Alamat rumah di",alamat)
+    print("Gender saya adalah",gender)
+    print("Umur saya",umur)
+    print("Hoby saya belakangan ini adalah",hoby)
+
+# memamnggil fungsi
+print(profil("Naila Fitriani Hidayat","Bogor","Perempuan","19 tahun","Melukis"))
